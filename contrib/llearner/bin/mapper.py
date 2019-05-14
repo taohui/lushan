8c0c5f672d55500f7dd25d362e438dd0a8021f62
taohui <taohui3@gmail.com>
2019-05-14 22:15:25 +0800

    remove llearner

--- contrib/llearner/bin/mapper.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-import sys
-import random
-
-for line in sys.stdin:
-	tokens = line.strip().split(' ')
-	print "%d\t%s"%(random.randint(0, 1000000000), '\t'.join(tokens))
--- contrib/llearner/bin/reducer.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-import sys
-
-for line in sys.stdin:
-	tokens = line.strip().split('\t')
-	print ' '.join(tokens[1:])
